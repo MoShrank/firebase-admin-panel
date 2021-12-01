6d66d9f18c6bb62ee06158820c80b515a809430e
Moritz Eich <hey@moritz.dev>
2021-12-01 21:33:05 +0100

    first commit + login page

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,59 @@
+import "./style.scss";
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { FormEvent, useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const auth = getAuth();
+
+  const login = async () => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      console.log(user);
+    } catch (error: any) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    login();
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
